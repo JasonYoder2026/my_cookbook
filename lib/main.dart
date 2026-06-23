cc93052288b788282f316b41e2bc8673db742148
jason <yoder453@gmail.com>
2026-06-23 10:55:50 -0400

    Loads env

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env.dev");
   runApp(const MyApp());
 }
